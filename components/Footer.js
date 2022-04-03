46efe1649311cd486f6211acc5c8e65bd004aeac
Kenny <spikelcdoj@gmail.com>
2022-04-03 12:55:59 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,9 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-    <a href="discord.gg/usafshippers">
-      <img alt="Join us on Discord!" src="https://i.imgur.com/7XCTfJM.png" />
+    <a href="https://discord.gg/usafshippers">
+      <img alt="Join us on Discord!" src="https://i.imgur.com/7XCTfJM.png" 
+        width="150" height= "150"/>
     </a>
     </>
   )
